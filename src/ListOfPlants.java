5d514f7ed2eea397303b80c681382a6e707a4812
Radim Sedláček <radim.sed@email.cz>
2024-06-04 22:22:20 +0200

    readFromFile in ListOfPlants

--- src/ListOfPlants.java
+++ src/ListOfPlants.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -25,7 +26,22 @@ public class ListOfPlants {
             Scanner scanner = new Scanner(bufferedReader);
             while(scanner.hasNextLine()){
                 String line = scanner.nextLine();
-                System.out.println(line);
+                String[] parts = line.split("\t");
+
+                String name = parts[0];
+                String description = parts[1];
+                LocalDate planted = LocalDate.parse(parts[4]);
+                LocalDate watering = LocalDate.parse(parts[3]);
+                int frequency = Integer.parseInt(parts[2]);
+
+                try {
+                    addPlant(new Plant(name, description, planted, watering, frequency));
+                } catch (PlantException e){
+                    System.err.println(e.getMessage());
+                }
+                for(String part : parts){
+                    System.out.println(part);
+                }
             }
 
         } catch ( IOException  e) {
