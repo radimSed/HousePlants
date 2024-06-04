3db59270ce3b332296f92f5270f9e07cf7613113
Radim Sedláček <radim.sed@email.cz>
2024-06-04 20:27:41 +0200

    Added setters/getters and getWateringInfo into Plant class

--- src/Plant.java
+++ src/Plant.java
@@ -1,2 +1,70 @@
+import java.time.LocalDate;
+
 public class Plant {
+    String name;
+    String notes;
+    LocalDate planted;
+    LocalDate watering;
+    int frequencyOfWatering;
+
+    public Plant(String name, String notes, LocalDate planted, LocalDate watering, int frequencyOfWatering) {
+        this.name = name;
+        this.notes = notes;
+        this.planted = planted;
+        this.watering = watering;
+        this.frequencyOfWatering = frequencyOfWatering;
+    }
+
+    public Plant(String name, LocalDate planted, int frequencyOfWatering) {
+        this(name, "", planted, LocalDate.now(),frequencyOfWatering );
+    }
+
+    public Plant(String name) {
+        this(name, "", LocalDate.now(), LocalDate.now(),7 );
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public LocalDate getPlanted() {
+        return planted;
+    }
+
+    public void setPlanted(LocalDate planted) {
+        this.planted = planted;
+    }
+
+    public LocalDate getWatering() {
+        return watering;
+    }
+
+    public void setWatering(LocalDate watering) {
+        this.watering = watering;
+    }
+
+    public int getFrequencyOfWatering() {
+        return frequencyOfWatering;
+    }
+
+    public void setFrequencyOfWatering(int frequencyOfWatering) {
+        this.frequencyOfWatering = frequencyOfWatering;
+    }
+
+    public String getWateringInfo(){
+        LocalDate dateOfNextWatering = this.watering.plusDays(frequencyOfWatering);
+        return this.name + ", " + this.watering + ", " + dateOfNextWatering;
+    }
 }
